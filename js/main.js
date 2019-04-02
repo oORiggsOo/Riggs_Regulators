a851d6c716025fe8ffb11cd5f3ff54abe519586f
o0Riggs0o <0Riggso@gmail.com>
2019-04-02 15:57:50 -0600

    image realignment and content for when and where

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Map it up
+function initMap() {
+  const loc = { lat: 51.132022, lng: -114.203847 };
+  const map = new google.maps.Map(document.querySelector('.map')
+  , {
+    zoom: 14,
+    center: loc
+      });
+const marker = new google.maps.marker({ position: loc, map:map});
+}
